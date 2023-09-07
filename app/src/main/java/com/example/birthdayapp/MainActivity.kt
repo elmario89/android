fdc31e6eaa6ebce438086ffa3a5edfc506fdc91d
Mikhail Smirnov <urine89@mail.ru>
2023-09-07 12:53:00 +0300

    Add center align and padding

--- app/src/main/java/com/example/birthdayapp/MainActivity.kt
+++ app/src/main/java/com/example/birthdayapp/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.birthdayapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthdayapp.ui.theme.BirthdayAppTheme
 
@@ -37,7 +40,10 @@ fun GreetingText(
     modifier: Modifier = Modifier,
     from: String = "Your Best Friend",
 ) {
-    Column(modifier = modifier) {
+    Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier.padding(20.dp)
+    ) {
         Text(
             text = message,
             fontSize = 80.sp,
